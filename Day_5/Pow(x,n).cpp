3addd458480b55ff2fda6e7f19a319453c2612a1
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-05 23:10:23 +0530

    problems day - 5

---
+++ Day_5/Pow(x,n).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double solve(double x, long long n)
+    {
+        if(n == 0){
+            return 1;
+        }
+
+        if(n == 1){
+            return x;
+        }
+
+        if(n % 2 == 0){
+            return solve(x*x, n/2);
+        }
+
+        return x*solve(x,n-1);
+    }
+
+    double myPow(double x, int n) {
+        long long num = n;
+        if(num < 0)
+            return (1.0 / solve(x,-1*num));
+        return solve(x,n);
+    }
+};
