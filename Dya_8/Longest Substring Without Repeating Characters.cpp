6d503a6e83f231fa6563618e4675638ccd5a245c
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-08 23:39:34 +0530

    problems day - 8

---
+++ Dya_8/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,24 @@
+class Solution{
+public:
+    int longestNonRepeatingSubstring(string& s){
+        int maxLen = 0;
+        int n = s.size();
+        int l = 0, r = 0;
+        vector<int> hash(256, -1);
+
+        while (r < n) {
+            if (hash[s[r]] != -1) {
+                if (hash[s[r]] >= l) {
+                    l = hash[s[r]] + 1;
+                }
+            }
+
+            int len = r - l + 1;
+            maxLen = max(maxLen, len);
+            hash[s[r]] = r;
+            r++;
+        }
+
+        return maxLen;
+    }
+};
