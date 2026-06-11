8808909dc9faf7780fda7afc934a5496055637f9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-11 10:49:38 +0530

    problems day - 11

--- Day_11/Detect a loop in LL.cpp
+++ Day_11/Detect a loop in LL.cpp
@@ -43,3 +43,49 @@ public:
         return false;
     }
 };
+
+// Optimal 
+
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head)
+            return false;
+
+        ListNode* slow = head, *fast = head;
+
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
