ce7eea9ab50ed06f5cc32ed4824774b7e08fbe8f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-12 22:46:10 +0530

    problems day - 12

---
+++ Day_12/Find the starting point in LL.cpp
@@ -0,0 +1,48 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode *findStartingPoint(ListNode *head) {
+        ListNode* slow = head, *fast = head;
+
+        while(fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+            {
+                slow = head;
+                while(slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
