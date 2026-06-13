ed4b318e2045f51126a54dcafd66dbcbc9f22971
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-13 13:32:39 +0530

    problems day - 13

--- Day_13/Rotate a LL.cpp
+++ Day_13/Rotate a LL.cpp
@@ -42,3 +42,70 @@ public:
         return head;
     }
 };
+
+// Optimal
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* findNthNode(ListNode* temp, int k)
+    {
+        int cnt = 1;
+        while(temp)
+        {
+            if(cnt == k)
+                return temp;
+            cnt++;
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next)
+            return head;
+
+        ListNode* tail = head;
+        int len = 1;
+        while(tail->next)
+        {
+            tail = tail->next;
+            len++;
+        }
+
+        if(k%len == 0)
+            return head;
+
+        k %= len;
+
+        tail->next = head;
+        ListNode* newLastNode = findNthNode(head, len - k);
+        head = newLastNode->next;
+        newLastNode->next = NULL;
+
+
+        return head;
+    }
+};
