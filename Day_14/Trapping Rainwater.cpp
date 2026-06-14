7cf8bb041e54484515031b5c9f49a51574b8e9ad
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-14 12:58:29 +0530

    problems day - 14

--- Day_14/Trapping Rainwater.cpp
+++ Day_14/Trapping Rainwater.cpp
@@ -38,3 +38,44 @@ public:
         return total;
     }
 };
+
+// Optimal
+class Solution
+{
+public:
+    int trap(vector<int> &height){
+        int n = height.size();
+        int total = 0;
+        int leftMax = 0, rightMax = 0;
+        int left = 0, right = n -1;
+
+        while(left < right)
+        {
+            if(height[left] <= height[right])
+            {
+                if(leftMax > height[left])
+                {
+                    total += (leftMax - height[left]);
+                }
+                else
+                {
+                    leftMax = height[left];
+                }
+                left += 1;
+            }
+            else
+            {
+                if(rightMax > height[right])
+                {
+                    total += (rightMax - height[right]);
+                }
+                else
+                {
+                    rightMax = height[right];
+                }
+                right -= 1;
+            }
+        }
+        return total;
+    }
+};
