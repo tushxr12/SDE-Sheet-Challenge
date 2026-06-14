13ea76fecbda455c1c1fe76afe5fb465d1bb5060
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-14 13:08:07 +0530

    problems day - 14

--- Day_14/Remove duplicates from sorted array.cpp
+++ Day_14/Remove duplicates from sorted array.cpp
@@ -7,3 +7,22 @@ public:
         return st.size();
     }
 };
+
+//Optimal
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0,j = 1;
+
+        for(int j = 1;j < n;j++)
+        {
+            if(nums[j] != nums[i])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return (i+1);
+    }
+};
