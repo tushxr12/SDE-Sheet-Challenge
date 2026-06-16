bcd428a3ecb63a3472924028443d2144323ba1d4
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-16 20:13:11 +0530

    problems day - 16

--- Day_16/Subset sum equals to target.cpp
+++ Day_16/Subset sum equals to target.cpp
@@ -24,3 +24,35 @@ public:
         return func(n-1, arr, target);
     }
 };
+
+// Memoized
+class Solution{   
+public:
+    bool func(int index, vector<int> &arr, int target, vector<vector<int>> &dp)
+    {
+        if(target == 0)
+            return true;
+
+        if(index == 0)
+        {
+            return (target == arr[index]);
+        }
+
+        if(dp[index][target] != -1)
+            return dp[index][target];
+
+        bool notTake = func(index - 1, arr, target, dp);
+
+        bool take = false;
+        if(target >= arr[index])
+            take = func(index-1,arr,target - arr[index], dp);
+
+        return dp[index][target] = (take || notTake);
+    }
+
+    bool isSubsetSum(vector<int>arr, int target){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1,-1));
+        return func(n-1, arr, target, dp);
+    }
+};
