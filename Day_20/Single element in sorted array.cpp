d07c87d6bd3c7a735c28ab52d88705e5d6ddd0dd
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-20 13:03:08 +0530

    problems day - 20

---
+++ Day_20/Single element in sorted array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int> &nums) {
+        unordered_map<int,int> mpp;
+        for(auto i : nums)
+            mpp[i]++;
+        
+        for(auto i : mpp)
+        {
+            if(i.second == 1)
+                return i.first;
+        }
+        return -1;
+    }
+};
