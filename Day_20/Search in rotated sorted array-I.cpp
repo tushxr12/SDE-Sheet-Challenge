bbd38079e25816927146370b08f1644c8dedaea9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-20 12:50:15 +0530

    problems day - 20

---
+++ Day_20/Search in rotated sorted array-I.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int> &nums, int k) {
+        int n = nums.size();
+        for(int i = 0;i < n;i++)
+        {
+            if(nums[i] == k)
+                return i;
+        }
+        return -1;
+    }
+};
