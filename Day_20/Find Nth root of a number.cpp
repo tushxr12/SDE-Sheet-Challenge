2f033897351cfdbcc084bdf4796ac99cc2f31a47
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-20 11:59:25 +0530

    problems day - 20

--- Day_20/Find Nth root of a number.cpp
+++ Day_20/Find Nth root of a number.cpp
@@ -13,3 +13,45 @@ public:
     return -1;
   }
 };
+
+
+// Optimal
+class Solution {
+public:
+  long long check(long long mid, int n, int m)
+  {
+    long long ans = 1;
+    for(int i = 1;i <= n;i++){
+        ans *= mid;
+        if(ans > m)
+            return 2;
+    }
+    if(ans == m)
+        return 1;
+    return 0;
+  }
+
+  int NthRoot(int N, int M) {
+    long long low = 1, high = M;
+
+    while(low <= high)
+    {
+        long long mid = (low + high)/2;
+        long long curr = check(mid,N,M);
+
+        if(curr == 1)
+        {
+            return mid;
+        }
+        else if(curr == 0)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+  }
+};
