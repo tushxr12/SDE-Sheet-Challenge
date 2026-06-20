105a5f33f4577f5cb232741231c2e285335fe065
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-20 13:06:28 +0530

    problems day - 20

--- Day_20/Single element in sorted array.cpp
+++ Day_20/Single element in sorted array.cpp
@@ -1,3 +1,4 @@
+// Using Map
 class Solution {
 public:
     int singleNonDuplicate(vector<int> &nums) {
@@ -9,6 +10,36 @@ public:
         {
             if(i.second == 1)
                 return i.first;
+        }
+        return -1;
+    }
+};
+
+// Implementation based
+class Solution {
+public:
+    int singleNonDuplicate(vector<int> &nums) {
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+
+        for(int i = 0;i < n;i++)
+        {
+            if(i == 0)
+            {
+                if(nums[i] != nums[i+1])
+                    return nums[i];
+            }
+            else if(i == n - 1)
+            {
+                if(nums[i] != nums[i-1])
+                    return nums[i];
+            }
+            else
+            {
+                if(nums[i] != nums[i-1] && nums[i] != nums[i+1])
+                    return nums[i];
+            }
         }
         return -1;
     }
