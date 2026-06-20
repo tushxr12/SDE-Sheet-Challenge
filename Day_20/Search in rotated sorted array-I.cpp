9665fbcb0cbaf2fba0c6df03f38ccbffae5b2869
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-20 12:59:03 +0530

    problems day - 20

--- Day_20/Search in rotated sorted array-I.cpp
+++ Day_20/Search in rotated sorted array-I.cpp
@@ -10,3 +10,46 @@ public:
         return -1;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    int search(vector<int> &nums, int k) {
+        int n = nums.size();
+        int low =0, high = n - 1;
+
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+
+            if(nums[mid] == k)
+                return mid;
+            
+            // Check if left half is sorted
+            if(nums[low] <= nums[mid])
+            {
+                if(k >= nums[low] && k <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            else
+            {
+                // Right sorted
+                if(k >= nums[mid] && k <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
