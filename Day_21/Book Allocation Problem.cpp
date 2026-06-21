1d49ed020c2d939883fde824802fe161bf892b65
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-21 12:43:54 +0530

    problems day - 21

--- Day_21/Book Allocation Problem.cpp
+++ Day_21/Book Allocation Problem.cpp
@@ -39,3 +39,55 @@ public:
         return low;
     }
 };
+
+// Optimal (Using BS)
+ class Solution {
+public:
+    int countStudents(vector<int> &nums, int pages)
+    {
+        int n = nums.size();
+
+        int students = 1;
+        int pagesStudent = 0;
+
+        for(int i = 0;i < n;i++)
+        {
+            if(pagesStudent + nums[i] <= pages)
+            {
+                pagesStudent += nums[i];
+            }
+            else
+            {
+                students += 1;
+                pagesStudent = nums[i];
+            }
+        }
+        return students;
+    }
+
+    int findPages(vector<int> &nums, int m)  {
+        int n = nums.size();
+
+        if(m > n)
+            return -1;
+        
+        int low = *max_element(begin(nums), end(nums));
+        int high = accumulate(begin(nums), end(nums), 0);
+
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            int students = countStudents(nums, mid);
+
+            if(students > m)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
