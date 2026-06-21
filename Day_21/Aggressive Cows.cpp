4b03245bb6fab958078f1d77cf881c5457533ada
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-21 13:47:42 +0530

    problems day - 21

--- Day_21/Aggressive Cows.cpp
+++ Day_21/Aggressive Cows.cpp
@@ -35,3 +35,50 @@ public:
         return (maxi - mini);
     }
 };
+
+// Optimal (Using BS)
+class Solution {
+public:
+    bool canWePlace(vector<int> &nums, int distance, int cows)
+    {
+        int cntCows = 1, last = nums[0];
+
+        for(int i = 1;i < nums.size();i++)
+        {
+            if(nums[i] - last >= distance)
+            {
+                cntCows++;
+                last = nums[i];
+            }
+            if(cntCows >= cows)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int aggressiveCows(vector<int> &nums, int k) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+        int maxi = *max_element(begin(nums), end(nums));
+        int mini = *min_element(begin(nums), end(nums));
+
+        int low = 1, high = (maxi - mini);
+
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+
+            if(canWePlace(nums, mid, k))
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+};
