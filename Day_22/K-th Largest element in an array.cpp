ee03f8ebc2184685da54c79fac5807ee2c121539
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-22 18:48:03 +0530

    problems day - 22

--- Day_22/K-th Largest element in an array.cpp
+++ Day_22/K-th Largest element in an array.cpp
@@ -5,3 +5,25 @@ public:
         return nums[k-1];
     }
 };
+
+// Using Min heap
+class Solution {
+public:
+    int kthLargestElement(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i = 0;i < k;i++)
+            pq.push(nums[i]);
+        
+        for(int i = k;i < nums.size();i++)
+        {
+            if(nums[i] > pq.top())
+            {
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+
+        return pq.top();
+    }
+};
