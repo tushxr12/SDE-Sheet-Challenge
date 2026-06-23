4af9c19bd340440906e38cf2c89d9b605301df73
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-23 21:30:18 +0530

    problems day - 23

--- Day_23/Top K Frequent Elements.cpp
+++ Day_23/Top K Frequent Elements.cpp
@@ -20,3 +20,34 @@ public:
         return ans;
     }
 };
+
+// Using heap
+class Solution {
+public:
+    vector<int> topKFrequent(const vector<int>& nums, int k) {
+        // Your code goes here
+        unordered_map<int,int> mpp;
+        for(auto i : nums)
+            mpp[i]++;
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> minHeap;
+
+        for(auto &i : mpp)
+        {
+            minHeap.push({i.second, i.first});
+
+            if(minHeap.size() > k)
+            {
+                minHeap.pop();
+            }
+        }
+
+        vector<int> ans;
+        while(!minHeap.empty())
+        {
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        return ans;
+    }
+};
