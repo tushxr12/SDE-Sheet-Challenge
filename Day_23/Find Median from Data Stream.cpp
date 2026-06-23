07bf8db7fb1fef9c5ef8c29930f4ffb9ca2e5e04
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-23 20:55:16 +0530

    problems day - 23

--- Day_23/Find Median from Data Stream.cpp
+++ Day_23/Find Median from Data Stream.cpp
@@ -30,3 +30,43 @@ public:
         }
     }
 };
+
+// Using heaps
+class MedianFinder {
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+
+        if(maxHeap.empty() || num <= maxHeap.top())
+        {
+            maxHeap.push(num);
+        }
+        else
+        {
+            minHeap.push(num);
+        }
+
+        if(maxHeap.size() > minHeap.size() + 1)
+        {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+        else if(minHeap.size() > maxHeap.size())
+        {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size() == minHeap.size())
+            return (maxHeap.top()*1.0 + minHeap.top()*1.0)/2.0;
+        return maxHeap.top();
+    }
+};
