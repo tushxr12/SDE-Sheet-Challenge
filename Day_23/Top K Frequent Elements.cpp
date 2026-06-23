f40a754699293f4b895cf9b2db48b60b48463253
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-23 21:23:53 +0530

    problems day - 23

---
+++ Day_23/Top K Frequent Elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> topKFrequent(const vector<int>& nums, int k) {
+        // Your code goes here
+        unordered_map<int,int> mpp;
+        for(auto i : nums)
+            mpp[i]++;
+        
+        vector<pair<int,int>> freqPairs(begin(mpp), end(mpp));
+        sort(begin(freqPairs),end(freqPairs), [](auto &a, auto &b){
+            return a.second > b.second;
+        });
+
+        vector<int> ans;
+        for(int i = 0;i < k;i++)
+        {
+            int num = freqPairs[i].first;
+            ans.push_back(num);
+        }
+        return ans;
+    }
+};
