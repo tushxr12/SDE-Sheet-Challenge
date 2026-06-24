69f0b3a907634d4583dd85930a002d70012fdc15
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-24 21:26:36 +0530

    problems day - 24

---
+++ Day_24/Implement Stack using Queue.cpp
@@ -0,0 +1,33 @@
+class QueueStack {
+    queue<int> q;
+public:
+    QueueStack() {
+    }
+    
+    void push(int x) {
+        int s = q.size();
+
+        q.push(x);
+
+        for(int i = 0;i < s;i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    
+    int pop() {
+        int n = q.front();
+        q.pop();
+        return n;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool isEmpty() {
+        return q.empty();
+    }
+};
