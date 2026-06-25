008669e20936dcf6b0259f5385e12cbcca623260
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-25 23:12:59 +0530

    problems day - 25

--- Day_25/Next Greater Element.cpp
+++ Day_25/Next Greater Element.cpp
@@ -1,3 +1,29 @@
+// Brute
+class Solution {
+public:
+    vector<int> nextLargerElement(vector<int> arr) {
+        int n = arr.size();
+        vector<int> ans(n,-1);
+
+        for(int i = 0;i < n;i++)
+        {
+            int currElement = arr[i];
+
+            for(int j = i + 1;j < n;j++)
+            {
+                if(arr[j] > currElement)
+                {
+                    ans[i] = arr[j];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
 class Solution {
 public:
     vector<int> nextLargerElement(vector<int> arr) {
