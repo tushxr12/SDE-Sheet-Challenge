1484a8a6c553725e2c77706baa68f75cf514e670
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-26 23:00:19 +0530

    problems day - 26

--- Day_26/Next Smaller Element.cpp
+++ Day_26/Next Smaller Element.cpp
@@ -19,3 +19,34 @@ public:
         return ans;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    vector<int> nextSmallerElements(const vector<int>& arr) {
+        // Your code goes here
+        int n = arr.size();
+        stack<int> st;
+
+        vector<int> ans(n, -1);
+
+        for(int i = n - 1;i >= 0;i--)
+        {
+            while(!st.empty() && st.top() >= arr[i]){
+                st.pop();
+            }
+
+            if(st.empty())
+            {
+                ans[i] = -1;
+            }
+            else
+            {
+                ans[i] = st.top();
+            }
+        
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
