5848b2d60dc9c4ed1d63af44c99bf407a8552953
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-02 23:23:22 +0530

    problems day - 32

---
+++ Day_32/Count and say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    string countAndSay(int n) {
+        if(n == 1)
+            return "1";
+        
+        string say = countAndSay(n-1);
+
+        string ans = "";
+
+        for(int i = 0;i < say.size();i++)
+        {
+            char ch = say[i];
+
+            int cnt = 1;
+
+            while(i < say.size() - 1 && say[i] == say[i+1]){
+                cnt++;
+                i++;
+            }
+
+            ans += to_string(cnt) + string(1,ch);
+        }
+
+        return ans;
+    }   
+};
