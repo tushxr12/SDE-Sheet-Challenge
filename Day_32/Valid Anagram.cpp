6010b5fcfa07c52dd2c468fe0a68a6950dcedccc
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-02 21:35:31 +0530

    problems day - 32

--- Day_32/Valid Anagram.cpp
+++ Day_32/Valid Anagram.cpp
@@ -7,3 +7,31 @@ class Solution{
             return (s == t);
 		}
 };
+
+// Better
+class Solution{	
+	public:
+		bool anagramStrings(string& s,string t){
+			//your code goes here
+            if(s.size() != t.size())
+                return false;
+            
+            vector<int> freq(26, 0);
+
+            for(char c : s)
+            {
+                freq[c-'a']++;
+            }
+
+            for(char c : t)
+            {
+                freq[c-'a']--;
+            }
+
+            for(auto i : freq){
+                if(i != 0)
+                    return false;
+            }
+            return true;
+		}
+};
