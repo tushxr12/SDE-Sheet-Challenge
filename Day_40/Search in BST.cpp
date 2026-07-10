4a746a36c916557f137765ad70cf39eb1775e724
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-10 23:23:44 +0530

    problems day - 40

---
+++ Day_40/Search in BST.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {	
+public:	
+    TreeNode* searchBST(TreeNode* root, int val) {
+    	//your code goes here
+        while(root && root->data != val){
+            root = (root->data > val) ? root->left : root->right;
+        }
+        return root;
+    }
+};
