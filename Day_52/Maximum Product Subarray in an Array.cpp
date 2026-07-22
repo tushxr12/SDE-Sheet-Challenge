a43962d28ad0a06e83f0f77efd70d64c30b5259e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-22 23:10:03 +0530

    problems day - 52

---
+++ Day_52/Maximum Product Subarray in an Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MIN;
+
+        int prefix = 1, suffix = 1;
+
+        for(int i = 0;i < n;i++)
+        {
+            if(prefix == 0)
+                prefix = 1;
+            if(suffix == 0)
+                suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            ans = max({ans, prefix, suffix});
+        }
+        return ans;
+    }
+};
