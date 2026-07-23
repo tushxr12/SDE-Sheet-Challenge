997f5369349d338c2a70035adf1739c60ba528a5
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-23 21:31:13 +0530

    problems day - 53

--- Day_53/0 and 1 Knapsack.cpp
+++ Day_53/0 and 1 Knapsack.cpp
@@ -27,3 +27,37 @@ class Solution{
             return func(n-1,W,wt,val);
         }
 };
+
+// Memoization
+class Solution{
+    private:
+        int func(int index, int weight, vector<int> &wt, vector<int> &val, vector<vector<int>> &dp)
+        {
+            if(index == 0)
+            {
+                if(weight >= wt[0])
+                    return val[0];
+                return 0;
+            }
+
+            if(dp[index][weight] != -1)
+                return dp[index][weight];
+
+            int notPick = 0 + func(index - 1,weight,wt,val,dp);
+
+            int pick = INT_MIN;
+
+            if(weight >= wt[index])
+            {
+                pick = val[index] + func(index-1,weight-wt[index],wt,val,dp);
+            }
+
+            return dp[index][weight] = max(pick, notPick);
+        }
+    public:
+        int knapsack01(vector<int>& wt, vector<int>& val, int n, int W) {
+            //your code goes here
+            vector<vector<int>> dp(n, vector<int>(W+1,-1));
+            return func(n-1,W,wt,val,dp);
+        }
+};
