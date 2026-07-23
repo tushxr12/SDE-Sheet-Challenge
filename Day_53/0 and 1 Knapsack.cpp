78114aa2dc0a262a6cceabda3a465888d25dc5b1
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-23 21:29:18 +0530

    problems day - 53

---
+++ Day_53/0 and 1 Knapsack.cpp
@@ -0,0 +1,29 @@
+// Recursion
+class Solution{
+    private:
+        int func(int index, int weight, vector<int> &wt, vector<int> &val)
+        {
+            if(index == 0)
+            {
+                if(weight >= wt[0])
+                    return val[0];
+                return 0;
+            }
+
+            int notPick = 0 + func(index - 1,weight,wt,val);
+
+            int pick = INT_MIN;
+
+            if(weight >= wt[index])
+            {
+                pick = val[index] + func(index-1,weight-wt[index],wt,val);
+            }
+
+            return max(pick, notPick);
+        }
+    public:
+        int knapsack01(vector<int>& wt, vector<int>& val, int n, int W) {
+            //your code goes here
+            return func(n-1,W,wt,val);
+        }
+};
