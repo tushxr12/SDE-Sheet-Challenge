f08063d86d55c10b7c999027847f52e0cf2bf575
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-25 12:32:28 +0530

    problems day - 54

--- Day_54/Coin change II.cpp
+++ Day_54/Coin change II.cpp
@@ -27,3 +27,36 @@ class Solution {
         return f(N-1,coins,amount);
     }
 };
+
+// Memoization
+class Solution {
+  private:
+    int mod = 1e9+7;
+    int f(int index, vector<int> &coins, int target, vector<vector<int>> &dp)
+    {
+        if(index == 0)
+        {
+            if(target%coins[index] == 0)
+                return 1;
+            return 0;
+        }
+
+        if(dp[index][target] != -1)
+            return dp[index][target];
+
+        int notTake = f(index-1,coins,target,dp);
+        int take = 0;
+
+        if(target >= coins[index])
+        {
+            take = f(index,coins,target-coins[index],dp); 
+        }
+
+        return dp[index][target] = (take+notTake)%mod;
+    }
+  public:
+   int count(vector<int>&coins, int N, int amount) {
+        vector<vector<int>> dp(N, vector<int>(amount+1,-1));
+        return f(N-1,coins,amount,dp);
+    }
+};
