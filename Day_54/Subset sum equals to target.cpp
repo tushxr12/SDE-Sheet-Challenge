d0866de938648de8bb0cf108c07975af4e8364ac
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-25 13:38:02 +0530

    problems day - 54

--- Day_54/Subset sum equals to target.cpp
+++ Day_54/Subset sum equals to target.cpp
@@ -1,3 +1,4 @@
+// Recursion
 class Solution{   
 private:
     bool f(int index, vector<int> &arr, int target)
@@ -27,5 +28,42 @@ public:
     bool isSubsetSum(vector<int>arr, int target){
         int n = arr.size();
         return f(n-1,arr,target);
+    }
+};
+
+// Memoization
+class Solution{   
+private:
+    bool f(int index, vector<int> &arr, int target, vector<vector<int>> &dp)
+    {
+        if(target == 0)
+            return true;
+
+        if(index == 0)
+        {
+            if(target == arr[index])
+                return true;
+            return false;
+        }
+
+        if(dp[index][target] != -1)
+            return dp[index][target];
+
+        bool notTake = f(index-1,arr,target,dp);
+
+        bool take = false;
+
+        if(target >= arr[index])
+        {
+            take = f(index-1,arr,target-arr[index],dp);
+        }
+
+        return dp[index][target] = (take || notTake);
+    }
+public:
+    bool isSubsetSum(vector<int>arr, int target){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(target+1,-1));
+        return f(n-1,arr,target,dp);
     }
 };
