aa71500b8984bf794c2b05592c2f05edc5ce8ff2
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-25 12:15:38 +0530

    problems day - 54

--- Day_54/Minimum Falling Path Sum.cpp
+++ Day_54/Minimum Falling Path Sum.cpp
@@ -31,3 +31,41 @@ public:
         return mini;
     }
 };
+
+// Memoization
+class Solution {
+private:
+    int f(int i,int j, int cols, vector<vector<int>> &matrix, vector<vector<int>> &dp)
+    {
+        if(j < 0 || j >= cols)
+            return 1e9;
+        
+        if(i == 0)
+            return matrix[i][j];
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int up = matrix[i][j] + f(i-1,j,cols,matrix,dp);
+        int leftDiag = matrix[i][j] + f(i-1,j-1,cols,matrix,dp);
+        int rightDiag = matrix[i][j] + f(i-1,j+1,cols,matrix,dp);
+
+        return dp[i][j] = min({up, leftDiag, rightDiag});
+    }
+
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<vector<int>> dp(rows, vector<int>(cols,-1));
+
+        int mini = INT_MAX;
+        for(int col = 0; col < cols;col++)
+        {
+            int ans = f(rows-1,col,cols,matrix,dp);
+            mini = min(ans, mini);
+        }
+        return mini;
+    }
+};
