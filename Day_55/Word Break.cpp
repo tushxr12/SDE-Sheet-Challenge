03f67aee1666b0f71978b539b8c15a132b941b7e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-25 14:10:16 +0530

    problems day - 55

---
+++ Day_55/Word Break.cpp
@@ -0,0 +1,36 @@
+// Recursion
+class Solution {
+private:
+    bool solve(int index, int n, string &s)
+    {
+        if(index >= n)
+            return true;
+        
+        if(st.find(s) != st.end())
+            return true;
+        
+        for(int l = 1;l <= n;l++)
+        {
+            string temp = s.substr(index,l);
+
+            if(st.find(temp) != st.end() && solve(index + l, n,s))
+            {
+                return true;
+            } 
+        }
+        return false;
+    }
+public:
+    unordered_set<string> st;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // Your code goes here
+        int n = s.size();
+
+        for(string &word : wordDict)
+        {
+            st.insert(word);
+        }
+
+        return solve(0,n,s);
+    }
+};
